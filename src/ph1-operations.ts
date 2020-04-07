581e574d569357a4213fe4cfd40c0ef6933da0ff
Alessandro Antonio Sangalli <alessandro.sangalli@pampa.compasso>
2020-04-07 08:33:51 -0300

    feat: create scratch of ph1 operations

--- src/ph1-operations.ts
+++ src/ph1-operations.ts
@@ -1,5 +1,74 @@
-import Operations from "./interface/operations";
+import Operation from "./interface/operations";
 
 export default class Ph1Operations implements Operation {
-  
+  nop() {}
+
+  ldr(value) {
+    const ac =  value
+  }
+
+  str() {
+
+  } 
+
+  add(value) {
+    let ac = 0
+    ac += value
+  } 
+
+  sub(value) {
+    let ac = 0
+    ac -= value
+  } 
+
+  mul(value) {
+    let ac = 0
+    ac *= value
+  } 
+
+  div(value) {
+    let ac = 0
+    ac /= value
+  } 
+
+  not(value) {
+    let ac = 0
+    ac = ~ac
+  } 
+
+  and(value) {
+    let ac = 0
+    ac = ac & value
+  } 
+
+  or(value) {
+    let ac = 0
+    ac = ac | value
+  }
+
+  xor(value) {
+    let ac = 0
+    ac = ac ^ value
+  }
+
+  jmp() {
+
+  }
+
+  jeq() {
+
+  }
+
+  jg() {
+
+  }
+
+  jl() {
+
+  }
+
+  hlt() {
+
+  }
+
 }
---
+++ src/ph1.ts
@@ -0,0 +1,9 @@
+export default class Ph1 {
+  public programCounter: number = 0
+  public acumulator: number = 0
+  public saida: Array<number> = [] 
+
+  Exmem() {
+
+  }
+}
