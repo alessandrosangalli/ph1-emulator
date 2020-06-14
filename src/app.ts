766b2e023ab73e5af0847ea4023db19ccf45906b
William Simionatto Nepomuceno <178344@upf.br>
2020-06-14 18:54:17 -0300

    feat: add integration of components

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+// import { PH1Emulator } from './ph1-emulator'
+// import { TxtReader } from './txt-reader'
+// const fileReader = new TxtReader()
+// const ph1Emulator = new PH1Emulator(fileReader)
+
+// async function exec (): Promise<void> {
+//   await ph1Emulator.setFile('./src/teste.txt')
+//   const result = ph1Emulator.execute()
+//   const accumulator = ph1Emulator.getAccumulator()
+//   const programCounter = ph1Emulator.getProgramCounter()
+// }
+
+// exec()
