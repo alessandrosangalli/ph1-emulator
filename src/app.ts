52eb52359893655287ca0d3f53d1431b24ad0fd8
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2020-06-14 22:36:26 -0300

    feat: add output of ph1emulator

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,51 @@
-// import { PH1Emulator } from './ph1-emulator'
-// import { TxtReader } from './txt-reader'
-// const fileReader = new TxtReader()
-// const ph1Emulator = new PH1Emulator(fileReader)
+import { PH1Emulator } from './ph1-emulator'
+import { TxtReader } from './txt-reader'
 
-// async function exec (): Promise<void> {
-//   await ph1Emulator.setFile('./src/teste.txt')
-//   const result = ph1Emulator.execute()
-//   const accumulator = ph1Emulator.getAccumulator()
-//   const programCounter = ph1Emulator.getProgramCounter()
-// }
+const fileReader = new TxtReader()
+const ph1Emulator = new PH1Emulator(fileReader)
+const numberToHex = (number: number): string => {
+  let hex = number.toString(16).toUpperCase()
+  const padding = 2
 
-// exec()
+  while (hex.length < padding) {
+    hex = `0${hex}`
+  }
+
+  return hex
+}
+
+async function startEmulator (): Promise<void> {
+  const inputFile: string = './src/teste.txt'
+  await ph1Emulator.setFile(inputFile)
+  const result = ph1Emulator.execute()
+  const accumulator = ph1Emulator.getAccumulator()
+  const programCounter = ph1Emulator.getProgramCounter()
+  console.log(`Input file: ${inputFile}`)
+  console.log('')
+  for (const line of result) {
+    const operator = line.operator === undefined ? '' : line.operator
+    const value = line.value === undefined ? '' : numberToHex(line.value)
+
+    console.log(`${operator} ${value}`)
+  }
+  console.log('')
+  console.log(`${result.length} instructions executed`)
+  console.log('')
+  console.log('Registers:')
+  console.log(`AC ${numberToHex(accumulator)}`)
+  console.log(`PC ${numberToHex(programCounter)}`)
+  console.log('')
+  let modifiedEntries = ''
+  for (const address in ph1Emulator.inputData) {
+    if (ph1Emulator.inputData[address].modified) {
+      const hexValue = numberToHex(ph1Emulator.inputData[address].value)
+      const hexAddress = numberToHex(parseInt(address))
+      modifiedEntries += `${hexAddress} ${hexValue}`
+    }
+  }
+  console.log('Memory:')
+  console.log(modifiedEntries.toString())
+}
+
+startEmulator()
+  .catch(err => console.log(err))
