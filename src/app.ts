159e89f1b367c5fd2c93b87bb37d7d86a27d195b
William <178344@upf.br>
2021-01-09 20:16:34 -0300

    complete function

--- src/app.ts
+++ src/app.ts
@@ -23,8 +23,8 @@ const invoices = {
 }
 
 function statament(invoice : any, plays : any) {
-    let totalAmount : Number = 0;
-    let volumeCredits : Number = 0;
+    let totalAmount : any = 0;
+    let volumeCredits : any = 0;
     let result : String = `Statament for ${invoice.customer}\n`;
     const format = new Intl.NumberFormat("en-us", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
@@ -34,18 +34,38 @@ function statament(invoice : any, plays : any) {
         
         switch(play.type) {
             case "tragedy":
-                thisAmount = 4000;
+                thisAmount = 40000;
                 if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30);
                 }
             break;
             case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20)
+                }
+
+                thisAmount += 300 * perf.audience;
+
             break;
             default:
                 throw new Error(`unknow type: ${play.type}`);
-            break;
         }
+
+        volumeCredits += Math.max(perf.audience - 30, 0);
+
+        if (play.type === "comedy") {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
+
+        result += `  ${play.name} : ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
     }
+
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+
+    return result;
 }
 
-statament(invoices, plays);
+console.log(statament(invoices, plays));
